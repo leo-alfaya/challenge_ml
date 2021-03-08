5ef563819e65c65770062e19ddcc4e91d4a71fe1
Leonardo Alfaya Fonseca <leonardo.fonseca@invillia.com>
2021-03-07 22:00:47 -0300

    feat: create project and getProducts function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const getProducts = ({ query }) => {
+    return fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
+      .then((response) => response.json())
+      .then((data) => [false, data])
+      .catch((error) => [true, error]);
+  };
+
+  useEffect(async () => {
+    const [hasError, result] = await getProducts({ query: "sansumg" });
+
+    if (!hasError) {
+      console.log("result => ", result);
+    } else {
+      console.log("error => ", result)
+    }
+  }, []);
+
+  return <p>App</p>;
+};
 
 export default App;
